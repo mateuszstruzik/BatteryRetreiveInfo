0cd4cc6ea2d69581a8b3970963bf836c14d366bb
Mati <26182867+mateuszstruzik@users.noreply.github.com>
2019-04-25 22:18:24 +0200

    initial commi, checking is WMi support CIM_BAttery

---
+++ BatteryRetreiveInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Management;
+
+namespace BatteryRetreiveInfo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectQuery query = new ObjectQuery("Select * from Win32_Battery");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection collection = searcher.Get();
+
+            foreach(var single in collection)
+            {
+                foreach(var prop in single.Properties)
+                {
+                    Console.WriteLine("Name :{0}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
